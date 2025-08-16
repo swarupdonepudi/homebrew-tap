060182309410b358cc448009feb004e524557f57
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-16 09:00:34 +0530

    releasing karayaml version v0.0.7 which replaced add with map

--- Formula/karayaml.rb
+++ Formula/karayaml.rb
@@ -1,5 +1,5 @@
 class Karayaml < Formula
-  version "v0.0.6"
+  version "v0.0.7"
   desc "YAML‑powered shortcut launcher for Karabiner‑Elements on macOS"
   homepage "https://github.com/swarupdonepudi/karayaml"
   os_arch = `arch`
