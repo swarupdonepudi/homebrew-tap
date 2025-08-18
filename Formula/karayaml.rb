f645b2e153d14d26fafd0262a852019f822e87ce
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-18 06:31:59 +0530

    bumped karayaml to v0.0.9 that has version command fixed

--- Formula/karayaml.rb
+++ Formula/karayaml.rb
@@ -1,5 +1,5 @@
 class Karayaml < Formula
-  version "v0.0.8"
+  version "v0.0.9"
   desc "YAML‑powered shortcut launcher for Karabiner‑Elements on macOS"
   homepage "https://github.com/swarupdonepudi/karayaml"
   os_arch = `arch`
