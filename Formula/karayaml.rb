29d5a81212fbf4060e3098ea598f79975ac7a004
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-20 09:12:27 +0530

    bumped karayaml to v0.0.14

--- Formula/karayaml.rb
+++ Formula/karayaml.rb
@@ -1,5 +1,5 @@
 class Karayaml < Formula
-  version "v0.0.13"
+  version "v0.0.14"
   desc "YAML‑powered shortcut launcher for Karabiner‑Elements on macOS"
   homepage "https://github.com/swarupdonepudi/karayaml"
   os_arch = `arch`
